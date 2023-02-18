23f73a55b4062311e55eaca3e2748231960ffd2e
Michael Tong <michael.tongwaiho@gmail.com>
2023-02-18 16:04:16 +0800

    testing commit

--- src/Home.js
+++ src/Home.js
@@ -17,6 +17,9 @@ const Home = ({ content }) => {
     </Helmet>      
       <div className="section1_container section1_bg">
         <HomeSection1 />
+        <div>
+          
+        </div>
       </div>
       <div className="section2_container section2_bg">
         <HomeSection2 />
