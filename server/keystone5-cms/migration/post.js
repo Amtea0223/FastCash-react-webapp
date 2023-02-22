a2878ae6914e8632af3eafc99e2dc761c9ef318f
Calvin Kwan <calvin.kwan@kazootechnology.com>
2023-02-22 23:41:26 +0800

    delete

--- server/keystone5-cms/migration/post.js
+++
@@ -1,9 +0,0 @@
-module.exports = [
-  {
-    data: {
-      key: 'initialSystem',
-      remark: 'Is the database is already initialized',
-      value: 'Done',
-    },
-  },
-];
--- server/keystone5-cms/views/components/logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const LogoImage = () => (
-  <img src="/file/images/logo.png" style={{ width: "38vw" }} />
-)
-
-export default LogoImage
